85aa7cb1bf5f447dcc5a3ce39a93eb0113ce3a1f
Sassid <redrednanaki@gmail.com>
2024-10-11 09:40:27 +0200

    added views in the same dir

---
+++ tuto-php/contact.php
@@ -0,0 +1,11 @@
+<?php 
+require 'header.php';
+?>
+
+<h2>Nous Contacter</h2>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sint adipisci voluptatem eum, eaque omnis porro, aliquam hic aspernatur explicabo est vel atque dolorem repellat. Quas doloremque cupiditate tenetur laborum.</p>
+
+<?php 
+require 'footer.php';
+?>
