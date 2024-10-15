5211c6360e3419f0219322481f939db1517f765d
Sassid <redrednanaki@gmail.com>
2024-10-15 13:50:26 +0200

    added blog folder

--- tuto-php/class/Creneau.php
+++ tuto-php/class/Creneau.php
@@ -1,10 +1,32 @@
-<?php 
+<?php
 
-class Creneau {
+class Creneau
+{
 
     public $debut;
 
     public $fin;
-}
 
-?> 
+    public function __construct(int $debut, int $fin)
+    {
+        $this->debut = $debut;
+        $this->fin = $fin;
+    }
+
+    public function inclusHeure(int $heure): bool
+    {
+        return $heure >= $this->debut && $heure <= $this->fin;
+    }
+
+    public function intersect(Creneau $creneau): bool
+    {
+        return $this->inclusHeure($creneau->debut) ||
+            $this->inclusHeure($creneau->fin) ||
+            ($this->debut > $creneau->debut && $this->fin < $creneau->fin);
+    }
+
+    public function toHTML(): string
+    {
+        return "<strong>{$this->debut}</stong> à <strong>{$this->fin}h</strong>";
+    }
+}
--- tuto-php/test.php
+++ tuto-php/test.php
@@ -3,8 +3,25 @@
 // require 'class/Creneau.php';
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . "Creneau.php";
 
-$creneau = new Creneau();
-$creneau->debut = 9;
-$creneau->fin = 12;
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . "Form.php";
 
-var_dump($creneau);
+$creneau = new Creneau(9, 12);
+$creneau2 = new Creneau(14, 16);
+
+var_dump($creneau, $creneau2);
+
+var_dump(
+    $creneau->inclusHeure(10),
+    $creneau2->inclusHeure(10)
+);
+
+var_dump($creneau->intersect($creneau2));
+
+echo $creneau->toHTML();
+
+// -----------------------------------------------
+
+
+echo Form::checkbox('demo', 'Demo', []);
+
+
